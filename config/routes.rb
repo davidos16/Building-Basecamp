6f3854dea04742406c274c9d645d1bc74249e6c2
dsanders_development <dsanders@them0ve.com>
2017-12-15 08:48:22 +0000

    Create & Index actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'project#index'
+  
+  
   get 'project/index'
+  post 'projects', to: 'project#create'
 
-root 'project#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
